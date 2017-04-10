dbf3f1657b35520cc38bffb806442dbc6d4af067
Dominic Clifton <me@dominicclifton.name>
2017-04-10 14:49:18 +0100

    Surface functional adder.

---
+++ src/test/scala/FunctionalAdderSpec.scala
@@ -0,0 +1,29 @@
+import org.scalatest.{FlatSpec, Matchers, WordSpec}
+
+class FunctionalSpec extends WordSpec with Matchers {
+
+  "functional adder" should {
+    "add" in {
+
+      import Converters._
+
+      var b = 1
+      var a = new FunctionalAdder(1)
+      a.add(b)
+
+      var result: Int = a
+      result shouldBe 2
+    }
+  }
+}
+
+object Converters {
+  implicit def converter(f: FunctionalAdder):Int = {
+    f.total
+  }
+}
+
+class FunctionalAdder(i: Int) {
+  var total = i
+  def add(b: Int) = total += b
+}
